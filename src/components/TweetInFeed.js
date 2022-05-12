11d2a3d04101034a623480ccf1385090cddfb884
Damon Pickett <damonpickett@Damons-Mac-mini.local>
2022-05-12 11:54:01 -0700

    added tweet feed with dummy content

--- src/components/TweetInFeed.js
+++ src/components/TweetInFeed.js
@@ -5,15 +5,138 @@ import canoe from '../images/canoe.jpeg';
 import { defaultImgs } from '../defaultimgs';
 import { Icon } from 'web3uikit';
 
-
 const TweetInFeed = () => {
-  
 
   return (
     <>
-    <div>
-      
+    <div className="feedTweet">
+      <img src={defaultImgs[0]} className='profilePic' alt='profile'></img>
+      <div className="completeTweet">
+        <div className="who">
+          Juhizzz
+          <div className="accWhen">0x42..314 * 1h</div>
+        </div>
+        <div className="tweetContent">
+          Nice day Golfing Today Shot 73 (+2)
+          <img src={golf} className="tweetImg" alt='golf'></img>
+        </div>
+        <div className="interactions">
+          <div className="interactionNums">
+            <Icon fill='#3f3f3f' size={20} svg='messageCircle' />
+          </div>
+          <div className="interactionNums">
+            <Icon fill='#3f3f3f' size={20} svg='star' />
+          </div>
+          <div className="interactionNums">
+            <Icon fill='#3f3f3f' size={20} svg='matic' />
+          </div>
+        </div>
+      </div>
     </div>
+    <div className="feedTweet">
+        <img src={defaultImgs[0]} className="profilePic" alt='d'></img>
+        <div className="completeTweet">
+          <div className="who">
+            Juhizzz
+            <div className="accWhen">0x42..314 · 1h</div>
+          </div>
+          <div className="tweetContent">
+            Nice Day Golfing Today Shot 73 (+2)
+            <img src={golf} className="tweetImg" alt='d'></img>
+          </div>
+          <div className="interactions">
+            <div className="interactionNums">
+              <Icon fill="#3f3f3f" size={20} svg="messageCircle" />
+            </div>
+            <div className="interactionNums">
+              <Icon fill="#3f3f3f" size={20} svg="star" />
+              12
+            </div>
+            <div className="interactionNums">
+              <Icon fill="#3f3f3f" size={20} svg="matic" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="feedTweet">
+        <img src={defaultImgs[0]} className="profilePic" alt='d'></img>
+        <div className="completeTweet">
+          <div className="who">
+            Juhizzz
+            <div className="accWhen">0x42..314 · 1h</div>
+          </div>
+          <div className="tweetContent">
+            is simply dummy text of the printing and typesetting industry. Lorem
+            Ipsum has been the industry's standard dummy text ever since the
+            1500s, when an unknown printer took a galley of type and scrambled
+            it to make a type specimen book. It has survived not only five
+            centuries, but also the leap into electronic typesetting, remaining
+            essentially un
+          </div>
+          <div className="interactions">
+            <div className="interactionNums">
+              <Icon fill="#3f3f3f" size={20} svg="messageCircle" />
+            </div>
+            <div className="interactionNums">
+              <Icon fill="#3f3f3f" size={20} svg="star" />
+              12
+            </div>
+            <div className="interactionNums">
+              <Icon fill="#3f3f3f" size={20} svg="matic" />
+            </div>
+          </div>
+        </div>
+      </div>
+      
+      <div className="feedTweet">
+        <img src={defaultImgs[0]} className="profilePic" alt='d'></img>
+        <div className="completeTweet">
+          <div className="who">
+            Juhizzz
+            <div className="accWhen">0x42..314 · 1h</div>
+          </div>
+          <div className="tweetContent">
+            Thoughts on the new Coca-Cola banana 🥤🍌 flavor?
+          </div>
+          <div className="interactions">
+            <div className="interactionNums">
+              <Icon fill="#3f3f3f" size={20} svg="messageCircle" />
+            </div>
+            <div className="interactionNums">
+              <Icon fill="#3f3f3f" size={20} svg="star" />
+              12
+            </div>
+            <div className="interactionNums">
+              <Icon fill="#3f3f3f" size={20} svg="matic" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="feedTweet">
+        <img src={defaultImgs[0]} className="profilePic" alt='d'></img>
+        <div className="completeTweet">
+          <div className="who">
+            Juhizzz
+            <div className="accWhen">0x42..314 · 1h</div>
+          </div>
+          <div className="tweetContent">
+            Love spending time on the water 🌊🌅
+            <img src={canoe} className="tweetImg" alt='d'></img>
+          </div>
+          <div className="interactions">
+            <div className="interactionNums">
+              <Icon fill="#3f3f3f" size={20} svg="messageCircle" />
+            </div>
+            <div className="interactionNums">
+              <Icon fill="#3f3f3f" size={20} svg="star" />
+              12
+            </div>
+            <div className="interactionNums">
+              <Icon fill="#3f3f3f" size={20} svg="matic" />
+            </div>
+          </div>
+        </div>
+      </div> 
     </>
   );
 };
